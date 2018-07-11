d05a884d7319c4b9dc195d32877a6904074b9d5f
Jakub Walczak <jakub.tomasz.walczak@gmail.com>
2018-07-11 23:23:05 +0200

    SpringBoot project created.

---
+++ src/main/java/pl/jakub/walczak/driveme/DrivemeApplication.java
@@ -0,0 +1,12 @@
+package pl.jakub.walczak.driveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivemeApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/jakub/walczak/driveme/DrivemeApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jakub.walczak.driveme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrivemeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
